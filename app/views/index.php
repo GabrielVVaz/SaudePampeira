a9c9e3f913f608b5f9fb2be0f1454a57fe794bcf
root <root@DESKTOP-4T0K40C>
2023-07-27 20:43:20 -0300

    teste-rota

---
+++ app/views/index.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\views\index;
+
+class Home {
+    public static function getHome(){
+        return "salve";
+    }
+}
---
+++ router.php
@@ -0,0 +1,5 @@
+<?php
+
+use \app\views\index;
+
+echo Home::getHome();
